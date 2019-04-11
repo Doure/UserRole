9b795491ec8ac0a12f4de63a5c6291780e2fea4c
Doure <gidroponikaim@gmail.com>
2019-04-11 21:08:32 +0300

    added repository and logic consoleApp

--- UserRoleConsoleApp/Program.cs
+++ UserRoleConsoleApp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DBLayer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,54 @@ namespace UserRoleConsoleApp
 {
     class Program
     {
+        /*        
+CREATE PROCEDURE [dbo].PR_GetUsersByRoleName
+			     @RoleName NVARCHAR(50)
+AS
+BEGIN
+	SELECT [User].UserName, [User].DateBirthday, [Role].RoleName, [Role].RoleId, [User].UserId
+	FROM [User]
+	INNER JOIN UserInRole ON UserInRole.UserId = [User].UserId
+	INNER JOIN [Role] ON [Role].RoleId = UserInRole.RoleId
+	WHERE [Role].RoleName = @RoleName
+END
+GO 
+        */
         static void Main(string[] args)
         {
+            try
+            {
+                Database.SetInitializer(new UserRolesInitializer());
+
+                using (IRepository repository = new UserRoleRepository())
+                {
+                    var managers = repository.GetUsersByRoleName("manager");
+
+                    if (managers.Count() > 0)
+                    {
+                        Console.WriteLine("Managers:");
+                        foreach (var manager in managers)
+                        {
+                            Console.WriteLine($"UserName: {manager.UserName} RoleName: {manager.RoleName} DateBirthday: {manager.DateBirthday}");
+                        }
+                    }
+                    
+                    var programmers = repository.GetUsersByRoleName("programmer");
+
+                    if (programmers.Count() > 0)
+                    {
+                        Console.WriteLine("Programmers:");
+                        foreach (var programmer in programmers)
+                        {
+                            Console.WriteLine($"UserName: {programmer.UserName} RoleName: {programmer.RoleName} DateBirthday: {programmer.DateBirthday}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ UserRoleConsoleApp/UserRolesRepository.cs
@@ -0,0 +1,103 @@
+﻿using DBLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRoleConsoleApp
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public DateTime DateBirthday { get; set; }
+        public string RoleName { get; set; }
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+    }
+    public interface IRepository : IDisposable
+    {
+        IEnumerable<User> GetUsers();
+        IEnumerable<Role> GetRoles();
+        IEnumerable<UserInRole> GetUsersInRoles();
+        IEnumerable<UserViewModel> GetUsersByRoleName(string roleName);
+    }
+    public class UserRoleRepository : IRepository
+    {
+        private const string getUsersByRoleNameProcedureName = "PR_GetUsersByRoleName";
+        private const string getUsersByRoleNameProcedureParamOneName = "RoleName";
+
+        private readonly UserRoleContext context;
+
+        public UserRoleRepository()
+        {
+            context = new UserRoleContext();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return context.Users;
+        }
+
+        public IEnumerable<Role> GetRoles()
+        {
+            return context.Roles;
+        }
+
+        public IEnumerable<UserViewModel> GetUsersByRoleName(string roleName)
+        {
+            SqlParameter parameterRoleName = new SqlParameter("@" + getUsersByRoleNameProcedureParamOneName, roleName)
+            {
+                SqlDbType = SqlDbType.NVarChar,
+                Size = 50
+            };
+
+            return context.Database.SqlQuery<UserViewModel>($"EXECUTE [dbo].{getUsersByRoleNameProcedureName} @{getUsersByRoleNameProcedureParamOneName}",
+                                                                                                                                       parameterRoleName).ToList();
+        }
+
+        /*
+        public IEnumerable<UserViewModel> GetUsersByRoleName2(string roleName)
+        {
+            return from user in context.Users
+                   join userInRole in context.UserInRoles on user.UserId equals userInRole.UserId
+                   join role in context.Roles on userInRole.RoleId equals role.RoleId
+                   where role.RoleName == roleName
+                   select new UserViewModel
+                   {
+                       RoleId = role.RoleId,
+                       UserId = user.UserId,
+                       DateBirthday = user.DateBirthday,
+                       RoleName = role.RoleName,
+                       UserName = user.UserName
+                   };
+        }
+        */
+        public IEnumerable<UserInRole> GetUsersInRoles()
+        {
+            return context.UserInRoles;
+        }
+    }
+}
